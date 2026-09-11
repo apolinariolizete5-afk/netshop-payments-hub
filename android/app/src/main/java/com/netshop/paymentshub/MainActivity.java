7f626e34f2bf2e208dd9b05974c2c78dda975951
apolinariolizete5-afk <apolinariolizete5@gmail.com>
2026-09-11 10:17:55 +0200

    Add Android MainActivity.java

---
+++ android/app/src/main/java/com/netshop/paymentshub/MainActivity.java
@@ -0,0 +1,11 @@
+package com.netshop.paymentshub;
+
+import android.os.Bundle;
+import com.capacitorjs.android.CapacitorActivity;
+
+public class MainActivity extends CapacitorActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
